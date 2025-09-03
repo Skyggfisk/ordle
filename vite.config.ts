49f121524898378f2ef79745f6d46fe2e182a10a
Martin Simonsen <marwynskyggfisk@gmail.com>
2025-09-03 12:05:34 +0000

    setup tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/ordle/", // important for GitHub Pages
+  plugins: [react(), tailwindcss()],
+  base: '/ordle/', // important for GitHub Pages
   build: {
-    outDir: "dist",
+    outDir: 'dist',
   },
 });
