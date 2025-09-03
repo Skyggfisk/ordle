89126f7d61377b641a1a92b8d72d667ec403ed83
Martin Simonsen <marwynskyggfisk@gmail.com>
2025-09-03 12:20:31 +0000

    add some helper dev scripts and run them

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/ordle/', // important for GitHub Pages
+  base: '/ordle/',
   build: {
     outDir: 'dist',
   },
