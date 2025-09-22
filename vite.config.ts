5d1dfccd818b57d1e3f373f493c17e03ae001725
Martin Simonsen <marwynskyggfisk@gmail.com>
2025-09-22 19:02:11 +0000

    attempt to patch broken plugin export

--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 import svgr from 'vite-plugin-svgr';
-// const VitePluginReactRemoveAttributes =
-//   // eslint-disable-next-line @typescript-eslint/no-require-imports
-//   require('vite-plugin-react-remove-attributes').default;
+import VitePluginReactRemoveAttributes from 'vite-plugin-react-remove-attributes';
 
 // https://vite.dev/config/
 export default defineConfig(async () => {
-  const { default: VitePluginReactRemoveAttributes } = await import(
-    'vite-plugin-react-remove-attributes'
-  );
-
   return {
     plugins: [
       react(),
