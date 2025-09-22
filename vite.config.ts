05239836d3a3e6710d2e0e73dc0728e23e636723
Martin Simonsen <marwynskyggfisk@gmail.com>
2025-09-22 18:27:50 +0000

    Broken vite plugin fix attempt 2

--- vite.config.ts
+++ vite.config.ts
@@ -2,22 +2,28 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 import svgr from 'vite-plugin-svgr';
-const VitePluginReactRemoveAttributes =
-  // eslint-disable-next-line @typescript-eslint/no-require-imports
-  require('vite-plugin-react-remove-attributes').default;
+// const VitePluginReactRemoveAttributes =
+//   // eslint-disable-next-line @typescript-eslint/no-require-imports
+//   require('vite-plugin-react-remove-attributes').default;
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-    svgr(),
-    VitePluginReactRemoveAttributes({
-      attributes: ['data-testid'],
-    }),
-  ],
-  base: '/ordle/',
-  build: {
-    outDir: 'dist',
-  },
+export default defineConfig(async () => {
+  const { default: VitePluginReactRemoveAttributes } = await import(
+    'vite-plugin-react-remove-attributes'
+  );
+
+  return {
+    plugins: [
+      react(),
+      tailwindcss(),
+      svgr(),
+      VitePluginReactRemoveAttributes({
+        attributes: ['data-testid'],
+      }),
+    ],
+    base: '/ordle/',
+    build: {
+      outDir: 'dist',
+    },
+  };
 });
